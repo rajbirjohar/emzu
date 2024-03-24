eaaec0a8dd6b3123ce9662aa240db2470fac14ef
rajbirjohar <rajbirjohar1@gmail.com>
2024-03-24 00:23:20 -0700

    init

---
+++ src/consts.ts
@@ -0,0 +1,14 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Emily Pai";
+export const SITE_DESCRIPTION = `
+Emily is a graphic designer and illustrator based in LA county with 
+an emphasis in layout design and packaging. To her, graphic design 
+is problem solving, and constantly a work in progress. She views her 
+work as a constant motivator to strive for improvement and discovering
+new ways of approaching design. No work is ever truly complete. When 
+she’s not designing, you might find her at the local bowling alley 
+aiming for a new personal best, or at home recharging by napping, 
+watching movies or playing videos games. 
+`;
---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from "astro:content";
+
+const pieces = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      subtitle: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.string().optional(),
+      images: z.array(
+        z.object({
+          src: image(),
+          alt: z.string(),
+        })
+      ),
+    }),
+});
+
+export const collections = { pieces };
